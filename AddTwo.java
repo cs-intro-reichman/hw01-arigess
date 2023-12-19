a2e75a9df7e8995c4daa8cf9fcfbfc4210f376aa
אריאל גס <arielgess@gmail.com>
2023-12-19 17:59:59 +0200

    change the print to a more elegant one

--- AddTwo.java
+++ AddTwo.java
@@ -5,6 +5,6 @@ public class AddTwo {
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        System.out.printf(a + " + " + b + " = " + (a + b));
+        System.out.printf("%d + %d = %d", a, b, (a+b));
     }
 }
