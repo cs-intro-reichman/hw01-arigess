2adfe4545ceebfaecdbe6b16313ab3b7eb66f00b
אריאל גס <arielgess@gmail.com>
2023-12-19 18:14:13 +0200

    Create a program that gets length of sides and prints whether it is a triangle

--- Triangle.java
+++ Triangle.java
@@ -1,10 +1,17 @@
 /*
- * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
- * This is known as the Triangle Inequality Theorem. 
+ * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side.
+ * This is known as the Triangle Inequality Theorem.
  * Write a program that tests if three given integers form a triangle.
- */ 
+ */
 public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+        int sideOne = Integer.parseInt(args[0]);
+        int sideTwo = Integer.parseInt(args[1]);
+        int sideThree = Integer.parseInt(args[2]);
+
+        boolean isTriangle = (sideOne + sideTwo > sideThree) && (sideOne + sideThree > sideTwo) && (sideTwo + sideThree > sideOne);
+
+        System.out.printf("%d, %d, %d: %s", sideOne, sideTwo, sideThree, isTriangle);
+
+    }
 }
