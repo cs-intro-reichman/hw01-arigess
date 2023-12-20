35bc11137255e47487addfc0705db1ac0c2efb28
אריאל גס <arielgess@gmail.com>
2023-12-20 20:44:08 +0200

    not using if now and supporting orderless parameters

--- GenThree.java
+++ GenThree.java
@@ -6,14 +6,16 @@ public class GenThree {
     public static final int RANDOM_GENERATIONS = 3;
 
     public static void main(String[] args) {
-        int lowerBound = Integer.parseInt(args[0]);
-        int upperBound = Integer.parseInt(args[1]);
+        int numberOne = Integer.parseInt(args[0]);
+        int numberTwo = Integer.parseInt(args[1]);
+        int lowerBound = Math.min(numberOne, numberTwo);
+        int upperBound = Math.max(numberOne, numberTwo);
 
         int minimalNumber = upperBound;
         for (int i = 0; i < RANDOM_GENERATIONS; i++) {
             int randomNumber = (int) Math.floor(Math.random() * (upperBound - lowerBound) + lowerBound);
             System.out.println(randomNumber);
-            if (randomNumber < minimalNumber) minimalNumber = randomNumber;
+            minimalNumber = Math.min(minimalNumber, randomNumber);
         }
 
         System.out.printf("The minimal generated number was %d", minimalNumber);
